104f56178ffefb6b22fbff113b9575f1124ecaaa
MartinCvetkovic <martincvetkovic2000@yahoo.com>
2022-06-03 12:00:28 +0200

    Dodata funkcionalnost za preuzimanje pdf-a, ali trenutno izlazi kao print pdf pop-up. Treba promeniti kasnije, cekam da mi jave mogu li biblioteke da se koriste

--- js/izgubljeni_ljubimci.js
+++ js/izgubljeni_ljubimci.js
@@ -19,6 +19,24 @@ $(document).ready(function(){
         inicijalizuj();
     }
 
+    function downloadPdf(){
+        let id = parseInt($(this).attr('id').slice(3, 4));
+
+        let win = window.open('', '', 'height=700,width=700');
+
+        win.document.write('<html><head></head><body>');
+
+        let obj = $($("#tbl" + id).html());
+        obj.children().last().remove();
+
+        win.document.write(obj.html());
+        win.document.write('</body></html>');
+
+        win.document.close();
+
+        win.print();
+    }
+
 
     function inicijalizuj(){
         let oglasi = JSON.parse(localStorage.getItem("oglasi"));
@@ -28,22 +46,24 @@ $(document).ready(function(){
         }
 
         oglasi.forEach((oglas, index) => {
-            let row = "<table class='table text-center'><tr><td>Ime ljubimca: </td><td>" + oglas.ime + "</td></tr><tr><td>Opis: </td><td>"
+            let row = "<table class='table text-center' id='tbl" + index + "'><tr><td>Ime ljubimca: </td><td>" + oglas.ime + "</td></tr><tr><td>Opis: </td><td>"
                 + oglas.opis + "</td></tr><tr><td>Kontakt telefon: </td><td>" + oglas.tel + "</td></tr>"
                 + "<tr><td><input class='form-control' id='inp" + index + "' type='text' placeholder='Tekst komentara'></td>"
                 +"<td><button class='comment-button btn btn-light btn-outline-secondary' id='btn" + index
-                + "'>Ostavi komentar</button></td></tr></table><h6 class='text-center'>Komentari</h6>";
+                + "'>Ostavi komentar</button></td><td><button class='pdf-button btn btn-light btn-outline-secondary' id='pdf" + index
+                + "'>Preuzmi oglas</button></td></tr></table><h6 class='text-center'>Komentari</h6>";
 
             $("#zivotinje").append(row);
 
             oglas.komentari.forEach(komentar=>{
-                let row = "<table class='table text-center'><tr><td>" + komentar.autor + "</td><td>" + komentar.tekst + "</td></tr></table>";
+                let row = "<table class='table text-left'><tr><td>" + komentar.autor + "</td><td>" + komentar.tekst + "</td></tr></table>";
 
                 $("#zivotinje").append(row);
             });
         });
 
         $(".comment-button").click(addComment);
+        $(".pdf-button").click(downloadPdf);
     }
 
     inicijalizuj();
