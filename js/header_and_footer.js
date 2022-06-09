477df22dfaaac38a515721d07bf2953370018c1a
MartinCvetkovic <martincvetkovic2000@yahoo.com>
2022-06-09 12:58:03 +0200

    Dizajn boja sredjen malo

--- js/header_and_footer.js
+++ js/header_and_footer.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
     ');
 
     let footer = $('\
-        <div class="col-12 text-center mt-5">\
+        <div class="col-12 text-center mt-5 rounded" id="footer">\
             Copyright 2022, Мартин Цветковић, Петар Тирнанић, Одсек за софтверско инжењерство Електротехничког факултета Универзитета у Београду\
         </div>\
     ');
