fb81b5ad18873b59b4043fc5f882ef63b28584cc
MartinCvetkovic <martincvetkovic2000@yahoo.com>
2022-06-13 00:14:12 +0200

    Sredjen dizajn i o_nama

--- js/index.js
+++ js/index.js
@@ -4,26 +4,26 @@ $(document).ready(function(){
         if(localStorage.getItem('oglasi') == null){
             let oglasi = [
                 {
-                    autor : 'Zika',
+                    autor : 'Žika',
                     ime : 'Bruno',
                     komentari : [
                         {
-                            autor: 'Nebojsa',
+                            autor: 'Nebojša',
                             tekst: 'Video sam zlatnog retrivera blizu Kalemegdana'
                         },
                         {
                             autor: 'Miki',
-                            tekst: 'Pronasao sam ga, kontaktirajte me na broj +1234567891'
+                            tekst: 'Pronašao sam ga, kontaktirajte me na broj +1234567891'
                         }
                     ],
-                    opis : 'Izgubljen zlatni retriver Bruno na Kalemegdanu, pronalazacu sledi nagrada',
+                    opis : 'Izgubljen zlatni retriver Bruno na Kalemegdanu, pronalazaču sledi nagrada',
                     tel : '+381123456789'
                 },
                 {
                     autor : 'Miki',
                     ime : 'Lora',
                     komentari : [],
-                    opis : 'Izgubljen zapadno skotski terijer kod pete skole u Vranju, hvala svima koji se jave',
+                    opis : 'Izgubljen zapadno škotski terijer kod pete škole u Vranju, hvala svima koji se jave',
                     tel : '+756735734'
                 }
             ];
